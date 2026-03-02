1e4476256c642b2afc4618908bf272a28cccadf2
vasilb7 <vasilminchevbenkov@gmail.com>
2026-03-02 03:27:37 +0200

    fix: prevent scroll to top when opening route-based modal

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -3,11 +3,15 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const location = useLocation();
 
   useEffect(() => {
+    // If we have a backgroundLocation, we're likely opening a modal over the current page.
+    // In this case, we don't want to scroll to the top.
+    if (location.state?.backgroundLocation) return;
+    
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [location.pathname, location.state]);
 
   return null;
 };
